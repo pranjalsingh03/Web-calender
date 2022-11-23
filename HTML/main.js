904b3ef5f153872867d571f9fbd8ec95a416538f
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-11-23 21:57:55 +0530

    javascript

---
+++ HTML/main.js
@@ -0,0 +1,26 @@
+let post=11
+function change_back(x){
+    document.getElementById(post).style.display='none'
+    post=post-1
+    if(post == -2){
+        post =12
+    }
+    document.getElementById(post).style.display='block'
+}
+function change_next(x){
+    document.getElementById(post).style.display='none'
+    post=post+1
+    if(post == 13){
+        post =-1
+    }
+    document.getElementById(post).style.display='block'
+}
+
+function holidays(){
+    document.getElementById('Quotes_div').style.display='none'
+    document.getElementById('Holiday_div').style.display='block'
+}
+function quotes(){
+    document.getElementById('Holiday_div').style.display='none'
+    document.getElementById('Quotes_div').style.display='block'
+}
