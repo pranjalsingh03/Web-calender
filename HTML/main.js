4c7591ac117d874a3ab9144cee7a8a1a16e6b9d3
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-12-02 11:33:26 +0530

    added calender

--- HTML/main.js
+++ HTML/main.js
@@ -2,7 +2,7 @@ let post=11
 function change_back(x){
     document.getElementById(post).style.display='none'
     post=post-1
-    if(post == -10){
+    if(post == -11){
         post =12
     }
     document.getElementById(post).style.display='block'
